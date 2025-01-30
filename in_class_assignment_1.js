ec88af30435ec8656c61711c6c9d97bf11724f1d
Ashley Le <ashleyle@usf.edu>
2025-01-30 16:36:29 -0500

    task 4and 5 completed :D

--- in_class_assignment_1.js
+++ in_class_assignment_1.js
@@ -26,3 +26,18 @@ if (score >= 60) {
 for (let i = 10; i >= 1; i--) {
     console.log(i); // printed numbers in descending order
 };
+
+// Task 4
+
+let num = 2;
+while (num <= 10) {
+    console.log(num);
+    num += 2;
+};
+
+// Task 5(?)
+
+function calculateSquare(number) {
+    return number * number;
+}
+console.log(calculateSquare(4));
